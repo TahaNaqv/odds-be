77c74fc2321401a57317232abeef04701edce6dd
Taha Abbas <taha.abbas@ideofuzion.com>
2025-05-27 15:56:06 +0500

    feat: transaction module

---
+++ src/transaction/transaction.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionService } from './transaction.service';
+import { Transaction } from '../entities/transaction.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Transaction])],
+  providers: [TransactionService],
+  exports: [TransactionService],
+})
+export class TransactionModule {}
+ 
---
+++ src/transaction/transaction.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Transaction } from '../entities/transaction.entity';
+
+@Injectable()
+export class TransactionService {
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionRepository: Repository<Transaction>,
+  ) {}
+
+  async save(transaction: Transaction): Promise<Transaction> {
+    return this.transactionRepository.save(transaction);
+  }
+}
+ 
